2bf58cf86c3c711f70d68a95fa2f87f90bcdf33c
bony-jose <73240707+bony-jose@users.noreply.github.com>
2020-10-22 08:01:28 +0530

    Create sum2.c

---
+++ sum2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,b,sum;
+
+printf("enter first operand");
+scanf("%d",&a);
+
+printf("enter second operand");
+scanf("%d",&b);
+
+sum=a+b;
+
+printf("Sum=%d",sum);
+}
